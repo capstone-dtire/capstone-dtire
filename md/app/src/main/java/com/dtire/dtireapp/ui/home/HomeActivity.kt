d0756ccddd76e43060b324f20883dc7a99f83169
ridho-dev <ridhopardede.dev@gmail.com>
2022-05-18 16:35:56 +0700

    Add Home Page

--- md/app/src/main/java/com/dtire/dtireapp/ui/home/HomeActivity.kt
+++ md/app/src/main/java/com/dtire/dtireapp/ui/home/HomeActivity.kt
@@ -1,8 +1,11 @@
 package com.dtire.dtireapp.ui.home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import com.dtire.dtireapp.databinding.ActivityHomeBinding
+import com.dtire.dtireapp.ui.login.LoginActivity
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
@@ -11,5 +14,16 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val toolbar: Toolbar = binding.tbHome
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = ""
+
+        binding.layoutHomeProfile.setOnClickListener {
+            val intent = Intent(this@HomeActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
