3d3c11a5b20bbae2a761605889551c7516b26a09
Allief Nuriman <57051116+alliefn@users.noreply.github.com>
2022-05-20 15:52:53 +0700

    Add: API for processing and getting user data, and API for adding detection history data

--- cc/back-end/src/router.js
+++ cc/back-end/src/router.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
-const { index, register, login } = require('./middleware');
+const { index, register, login, getUser, getUserDetails, editUserDetails, editUserNameOrEmail, addDetectionHistory } = require('./middleware');
 
 router.use(bodyParser.json());
 
 router.get('/', index);
 router.post('/api/register', register);
 router.post('/api/login', login);
+router.get('/api/user/:user_id', getUser);
+router.get('/api/user_details/:user_id', getUserDetails);
+router.put('/api/user_details/:user_id', editUserDetails);
+router.put('/api/user/:user_id', editUserNameOrEmail);
+router.post('/api/detection_history', addDetectionHistory);
 
 module.exports = router;
