99791ba543e5342783925c3e24b512eff5a6fbb4
ridho-dev <ridhopardede.dev@gmail.com>
2022-05-22 10:09:23 +0700

    Improve Camera and Result page

--- md/app/src/main/java/com/dtire/dtireapp/ui/home/HomeActivity.kt
+++ md/app/src/main/java/com/dtire/dtireapp/ui/home/HomeActivity.kt
@@ -1,24 +1,30 @@
 package com.dtire.dtireapp.ui.home
 
 import android.Manifest
+import android.app.Dialog
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.view.*
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
+import com.dtire.dtireapp.R
 import com.dtire.dtireapp.databinding.ActivityHomeBinding
-import com.dtire.dtireapp.ui.login.LoginActivity
 import com.dtire.dtireapp.ui.result.ResultActivity
 import com.dtire.dtireapp.utils.createTempFile
+import com.dtire.dtireapp.utils.uriToFile
 import java.io.File
+
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
@@ -42,15 +48,41 @@ class HomeActivity : AppCompatActivity() {
             )
         }
 
-        binding.layoutHomeProfile.setOnClickListener {
-            val intent = Intent(this@HomeActivity, LoginActivity::class.java)
-            startActivity(intent)
+        binding.apply {
+            layoutHomeProfile.setOnClickListener {  }
+            layoutHomeToCamera.setOnClickListener { showDialog() }
+            layoutHomeToMap.setOnClickListener {  }
+            layoutHomeToHistory.setOnClickListener {  }
         }
-        binding.layoutHomeToHistory.setOnClickListener {
-            val intent = Intent(this@HomeActivity, LoginActivity::class.java)
-            startActivity(intent)
+
+    }
+
+    private fun showDialog() {
+        val dialog = Dialog(this)
+        dialog.apply {
+            requestWindowFeature(Window.FEATURE_NO_TITLE)
+            setCancelable(false)
+            setContentView(R.layout.dialogsheetlayout)
+            window?.apply {
+                setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+                setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                navigationBarColor = ContextCompat.getColor(this@HomeActivity, R.color.white)
+                setGravity(Gravity.CENTER)
+            }
+            show()
         }
-        binding.layoutHomeToCamera.setOnClickListener { startCamera() }
+
+        val toCamera = dialog.findViewById(R.id.bs_home_to_camera) as LinearLayout
+        val toGallery = dialog.findViewById(R.id.bs_home_to_gallery) as LinearLayout
+
+        toCamera.setOnClickListener {
+            startCamera()
+            dialog.dismiss()
+        }
+        toGallery.setOnClickListener {
+            startGallery()
+            dialog.dismiss()
+        }
     }
 
     private fun startCamera() {
@@ -68,14 +100,33 @@ class HomeActivity : AppCompatActivity() {
         }
     }
 
+    private fun startGallery() {
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Choose a Picture")
+        launcherIntentGallery.launch(chooser)
+    }
+
     private val launcherIntentCamera = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
     ) {
         if (it.resultCode == RESULT_OK) {
             val myFile = File(currentPhotoPath)
             getFile = myFile
-            val result = BitmapFactory.decodeFile(myFile.path)
-            // send result to summary page here
+            val intent = Intent(this@HomeActivity, ResultActivity::class.java)
+            intent.putExtra(ResultActivity.EXTRA_IMAGE, myFile.path)
+            startActivity(intent)
+        }
+    }
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val selectedImg: Uri = result.data?.data as Uri
+            val myFile = uriToFile(selectedImg, this@HomeActivity)
+            getFile = myFile
             val intent = Intent(this@HomeActivity, ResultActivity::class.java)
             intent.putExtra(ResultActivity.EXTRA_IMAGE, myFile.path)
             startActivity(intent)
--- md/app/src/main/java/com/dtire/dtireapp/ui/imagedetail/ImageDetailActivity.kt
+++ md/app/src/main/java/com/dtire/dtireapp/ui/imagedetail/ImageDetailActivity.kt
@@ -3,8 +3,9 @@ package com.dtire.dtireapp.ui.imagedetail
 import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 import com.dtire.dtireapp.databinding.ActivityImageDetailBinding
-import com.dtire.dtireapp.databinding.ActivityResultBinding
 import com.dtire.dtireapp.ui.result.ResultActivity
 
 class ImageDetailActivity : AppCompatActivity() {
@@ -15,10 +16,24 @@ class ImageDetailActivity : AppCompatActivity() {
         binding = ActivityImageDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val toolbar: Toolbar = binding.tbImageDetail
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         val picturePath = intent.getStringExtra(ResultActivity.EXTRA_IMAGE)
         val result = BitmapFactory.decodeFile(picturePath)
         binding.ivImageDetail.setImageBitmap(result)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> finish()
+            else -> finish()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
     companion object {
         const val EXTRA_IMAGE = "extra_image"
     }
