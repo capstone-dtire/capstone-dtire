bec5c4633159911040d92f9801c641ab65bd1fe5
Allief Nuriman <57051116+alliefn@users.noreply.github.com>
2022-05-26 15:02:06 +0700

    Add: Body-parser to support x-www-form-urlencoded

--- cc/back-end/src/app.js
+++ cc/back-end/src/app.js
@@ -2,12 +2,20 @@ const express = require('express');
 var cors = require('cors');
 const app = express();
 const routes = require('./router');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
 app.use(cors({
     // allow CORS from the env variable
     origin: process.env.CORS_ORIGIN
 }));
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 app.use(routes);
 
 app.listen(3000, function () {
