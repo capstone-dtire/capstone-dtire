3bcd799ea45468d04f71cb521168c84b262a488e
ridho-dev <ridhopardede.dev@gmail.com>
2022-06-09 11:15:57 +0700

    Add splash screen

--- md/app/src/main/java/com/dtire/dtireapp/ui/initialScreen/SplashScreenActivity.kt
+++ md/app/src/main/java/com/dtire/dtireapp/ui/initialScreen/SplashScreenActivity.kt
@@ -9,20 +9,29 @@ import android.os.Looper
 import android.view.WindowManager
 import androidx.core.content.ContextCompat
 import com.dtire.dtireapp.R
+import com.dtire.dtireapp.data.preferences.UserPreference
 import com.dtire.dtireapp.ui.home.HomeActivity
+import com.dtire.dtireapp.ui.login.LoginActivity
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var preference: UserPreference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        window.navigationBarColor = ContextCompat.getColor(this, R.color.white)
+        preference = UserPreference(this)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 500)
+            if (preference.isLoggedIn()) {
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }, 1500)
     }
 }
